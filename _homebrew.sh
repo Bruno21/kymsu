e9db6f187d91babfd9c2fea21910ba64b8d55c47
Bruno 21 <bruno@clicclac.info>
2018-03-13 06:33:17 +0100

    Clean up _homebrew.sh

--- _homebrew.sh
+++ _homebrew.sh
@@ -3,10 +3,6 @@
 # Homebrew plugin for KYMSU
 # https://github.com/welcoMattic/kymsu
 
-if [[ "${1}" == "latest" ]]; then
-	echo "toto"
-fi
-
 echo "🍺  Homebrew"
 brew update
 
@@ -15,7 +11,6 @@ echo ""
 brew_outdated=$(brew outdated)
 upd3=$(echo "$brew_outdated" | awk '{print $1}')
 #brewsy=$(echo "$brew_outdated" | wc -l | awk {'print $1'})
-echo ":$upd3:"
 
 #if [ "$upd3" != "" ]; then
 if [ -n "$upd3" ]; then
@@ -42,7 +37,6 @@ else
 	echo -e "\033[4mNo availables Cask updates.\033[0m"
 fi
 
-#if [[ $1 == "latest" ]]; then
 echo ""
 latest=$(echo "$cask_outdated" | grep '(latest)')
 if [ -n "$latest" ]; then
@@ -58,7 +52,6 @@ if [ -n "$latest" ]; then
   	esac
 
 fi
-#fi
 echo ""
 
 echo "🍺  ️The Doc is checking that everything is ok."
