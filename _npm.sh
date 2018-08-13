0960dd6ee44b1380cf876e2ca0c08afd3af3a029
Bruno 21 <bruno@clicclac.info>
2018-08-13 16:34:00 +0200

    _npm.sh
    -add support for global installed packages
    -no distract mode (for glabal packages)

--- _npm.sh
+++ _npm.sh
@@ -16,15 +16,43 @@
 
 # https://github.com/npm/npm/issues/17744
 
+# No distract mode
+no_distract=false
+
+local_path=/Users/bruno/Sites/node_modules/
+
 echo -e "\033[1m🌿  npm \033[0m"
 echo ""
 
-cd /Users/bruno/Sites/node_modules/
-echo -e "\033[4mInstalled scripts:\033[0m"
+# Local packages
+#cd /Users/bruno/Sites/node_modules/
+cd $local_path
+echo -e "\033[4mLocal installed scripts:\033[0m"
 npm ls
 outdated=$(npm outdated)
-echo "$outdated" | awk '{print $1}' | xargs npm update
+if [ -n "$outdated" ]; then
+	echo "$outdated"
+	echo "$outdated" | awk '{print $1}' | xargs npm update
+else
+	echo -e "\033[4mNo local packages updates.\033[0m"
+fi
+	
 echo ""
+
+# Global packages
+echo -e "\033[4mGlobal installed scripts:\033[0m"
+npm list -g --depth=0
+g_outdated=$(npm outdated -g --depth=0)
+if [ -n "$g_outdated" ]; then
+	if [ "$no_distract" = false ]; then
+		echo "$g_outdated"
+		echo "$g_outdated" | sed '1d' | awk '{print $1}' | xargs -p -n 1  npm install -g 
+	else
+		echo "$g_outdated" | sed '1d' | awk '{print $1}' | xargs -n 1  npm install -g
+	fi
+else
+	echo -e "\033[4mNo global packages updates.\033[0m"
+fi
 
 if [[ $1 == "--npm_cleanup" ]]; then
 	echo "🌬  Cleaning npm cache"
@@ -33,3 +61,7 @@ if [[ $1 == "--npm_cleanup" ]]; then
 fi
 
 echo ""
+
+
+
+
