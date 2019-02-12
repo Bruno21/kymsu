a49c5a79c09f01fd30a913e3db39c7099115f44f
Bruno 21 <bruno@clicclac.info>
2019-02-12 17:56:44 +0100

    _pecl.sh
    doc.php.net, pecl.php.net
    pecl upgrade package
    
    pear.php.net
    pecl channel-update pear.php.net
    
    toujours en test

--- _pecl.sh
+++ _pecl.sh
@@ -9,7 +9,7 @@ echo -e "\033[1m🐘 pecl \033[0m"
 
 echo ""
 
-echo -e "\033[1m❗️ plugin en test (pre-alpha) \033[0m"
+echo -e "\033[1m❗️ plugin en test (alpha) \033[0m"
 echo ""
 
 #upd=$(echo "$pip_outdated" | sed '1,2d' | awk '{print $1}')
@@ -36,9 +36,43 @@ pecl_upgrade=$(echo "$upgrade")
 if [ -n "$pecl_upgrade" ]; then
 	
 	echo -e "\033[4mExtensions update:\033[0m"
+	
 	echo ""
+	# à supprimer
 	echo "$pecl_upgrade"
+	# / à supprimer
+
+	echo ""
+	available=$(echo "$upgrade" | grep -v 'No upgrades available' | grep 'kB')
+	# pecl.php.net APCu    5.1.16 (stable) 5.1.17 (stable) 93kB
+
+	while read ligne 
+	do 
+		echo "$ligne"
+		a=$(echo "$ligne" | grep "pear")
+		if [ -n "$a" ]; then
+			echo "pear update available"
+			# pecl channel-update pear.php.net
+		else
+			#echo "pecl or doc update available"
+			pecl=true
+			b=$(echo "$ligne" | awk '{print $2}')
+			pecl info "$b"
+			#pecl upgrade "$b"
+		fi
+	done <<< "$available"
+
 fi
 
 echo ""
 echo ""
+
+#channels=$(pecl list-channels | sed '1,3d;$d' | grep -E '.com|.net' | awk '{print $1}')
+#for i in $channels
+#do
+#	echo "$i"
+	# pecl channel-update $i
+#done
+
+# WARNING: channel "pear.php.net" has updated its protocols, 
+#   use "pecl channel-update pear.php.net" to update
